e501fcb27f31089810a7f46b24f490d21739fc0e
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-08-02 21:00:10 +0300

    Update nft_mint.ts

--- ts/cluster1/nft_mint.ts
+++ ts/cluster1/nft_mint.ts
@@ -2,7 +2,7 @@ import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createSignerFromKeypair, signerIdentity, generateSigner, percentAmount } from "@metaplex-foundation/umi"
 import { createNft, mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import base58 from "bs58";
 
 const RPC_ENDPOINT = "https://api.devnet.solana.com";
@@ -16,11 +16,24 @@ umi.use(mplTokenMetadata())
 const mint = generateSigner(umi);
 
 (async () => {
+    const tx = createNft(umi, {
+        mint,
+        name: "Yusuf",
+        symbol: "Shooter",
+        uri: "https://arweave.net/jfYVGPoD__oDPYX8fK3ohf0-4N3zrOIjriOuBHe_JFo",
+        sellerFeeBasisPoints: percentAmount(20.24),
+    });
+
+    const result = await tx.sendAndConfirm(umi);
+    const signature = base58.encode(result.signature);
+
+    console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
+
+    console.log("Mint Adress: ", mint.publicKey);
     // let tx = ???
     // let result = await tx.sendAndConfirm(umi);
     // const signature = base58.encode(result.signature);
     
     // console.log(`Succesfully Minted! Check out your TX here:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`)
 
-    console.log("Mint Address: ", mint.publicKey);
 })();
