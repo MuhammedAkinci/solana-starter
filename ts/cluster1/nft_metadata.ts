5231b65e155bdeff663c2a23802b28a461984fa1
Muhammed Akıncı <95518574+MuhammedAkinci@users.noreply.github.com>
2024-08-02 20:59:57 +0300

    Update nft_metadata.ts

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../wba-wallet.json"
+import wallet from "./wallet/wba-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
 import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
@@ -16,6 +16,28 @@ umi.use(signerIdentity(signer));
     try {
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
+
+        const imageUri = "https://arweave.net/y9KA_uB7EzG5lYUSH72GgeOe8FZeI09JIhAeB6QSo5o"
+        const metadata = {
+            name: "Yusuf",
+            symbol: "Shooter",
+            description: "This shooter is on fire.",
+            image: imageUri,
+            attributes: [
+                {trait_type: 'NotABootcamp', value: 'true'},
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/jpeg",
+                        uri: imageUri,
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your metedata URI: ", myUri);
 
         // const image = ???
         // const metadata = {
